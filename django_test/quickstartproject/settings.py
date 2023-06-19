81355f733a8656b416eb6d94e4c385e366cb3ce3
Kacper <rybkowski84@gmail.com>
2023-06-19 22:41:26 +0200

    flex

--- django_test/quickstartproject/settings.py
+++ django_test/quickstartproject/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS = ["https://weatherappproject.azurewebsites.net/hisdata/", "https://weatherappproject.azurewebsites.net/index/"]
+CSRF_TRUSTED_ORIGINS = ["https://weatherappproject.azurewebsites.net"]
 
 
 # Quick-start development settings - unsuitable for production
